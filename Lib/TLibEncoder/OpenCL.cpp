c1b690ee1bfc014c322e31f1923216ec0acd9421
gomezpirry <gomezpirry@hotmail.com>
2015-04-02 17:49:19 -0500

    Commit message

---
+++ Lib/TLibEncoder/OpenCL.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   OpenCL.cpp
+ * Author: augusto
+ * 
+ * Created on March 30, 2015, 5:59 PM
+ */
+
+#include "OpenCL.h"
+
+OpenCL::OpenCL() {
+}
+
+OpenCL::OpenCL(const OpenCL& orig) {
+}
+
+OpenCL::~OpenCL() {
+}
+
---
+++ Lib/TLibEncoder/OpenCL.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   OpenCL.h
+ * Author: augusto
+ *
+ * Created on March 30, 2015, 5:59 PM
+ */
+
+#ifndef OPENCL_H
+#define	OPENCL_H
+
+class OpenCL {
+public:
+    OpenCL();
+    OpenCL(const OpenCL& orig);
+    virtual ~OpenCL();
+private:
+
+};
+
+#endif	/* OPENCL_H */
+
